0cfb537ccdf7b54b9432d4737d92f7626578f579
Johnson Fernandez <brojohnsonfernandez@gmail.com>
2024-09-24 16:34:12 +0530

    new commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],  // Add 'jsx' extension here
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
